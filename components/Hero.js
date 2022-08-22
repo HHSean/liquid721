33c8d347081f2f00098fef26648a3d312b52fb8b
joc-rgb <89299412+joc-rgb@users.noreply.github.com>
2022-08-23 00:57:05 +0800

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -35,11 +35,7 @@ export default function Hero() {
             </Text>{" "}
             For Your NFTs
           </Heading>
-          <Text color={"gray.500"}>
-            Monetize your content by charging your most loyal readers and reward
-            them loyalty points. Give back to your loyal readers by granting
-            them access to your pre-releases and sneak-peaks.
-          </Text>
+          
           <Stack
             direction={"column"}
             spacing={3}
