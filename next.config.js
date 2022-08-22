f8130659d2811cddfd0bed31733c16f2b144db89
jocxyen <bestoneisme123@gmail.com>
2022-08-23 00:06:36 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = {
+  ...nextConfig,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  trailingSlash: true,
+  path: "",
+};
